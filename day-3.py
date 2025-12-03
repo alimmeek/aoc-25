2c8eccae968f91f243589b15fd02879c121fff8b
Alia Meek <69384169+alimmeek@users.noreply.github.com>
2025-12-03 11:29:55 +0000

    Day 3 done!

---
+++ day-3.py
@@ -0,0 +1,50 @@
+def process_file():
+    with open("input.txt", "r") as f:
+        battery_packs = [line.strip('\n') for line in f.readlines()]
+    
+    return battery_packs
+
+
+def pt1(battery_packs: list[list[str]]):
+    sum = 0
+
+    for pack in battery_packs:
+        largest = sorted(pack, reverse=True)[0]
+        largest_index = pack.index(largest)
+
+        if (largest_index == len(pack)-1):
+            second = sorted(pack, reverse=True)[1]
+            joltage = int(str(second) + str(largest))
+        else:
+            second = sorted(pack[largest_index+1:], reverse=True)[0]
+            joltage = int(str(largest) + str(second))
+        
+        sum += joltage
+
+    return sum
+
+
+def pt2(battery_packs: list[str]):
+    total = 0
+    for pack in battery_packs:
+        pack_i = [int(x) for x in pack]
+
+        final_num = [0 for _ in range(12)]
+        positions = [0 for _ in range(12)]
+
+        final_num[0] = max(pack_i[:-11])
+        positions[0] = pack_i.index(final_num[0])
+        pack_i[positions[0]] = 0
+
+        for i in range(1, 11):
+            final_num[i] = max(pack_i[positions[i-1]+1:][:-11 + i])
+            positions[i] = positions[i-1]+1+pack_i[positions[i-1]+1:].index(final_num[i])
+            pack_i[positions[i]] = 0
+        final_num[-1] = max(pack_i[positions[-2]+1:])
+        
+        total += int("".join([str(digit) for digit in final_num]))
+    return total
+
+
+if __name__ == "__main__":
+    print(pt2(process_file()))
